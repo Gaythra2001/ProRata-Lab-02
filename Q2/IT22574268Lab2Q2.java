e5a919ad317fe9c1a0f7bf5eb8aec01c38e45ab0
Gayathra Malshan <139929718+Gaythra2001@users.noreply.github.com>
2026-09-24 21:44:04 +0530

    IT22574268Lab2Q2

---
+++ Q2/IT22574268Lab2Q2.java
@@ -0,0 +1,11 @@
+public class IT22574268Lab2Q2 {
+    public static void main(String[] args) {
+        double length = 10.0;
+        double perimeter = 4 * length;
+        double pi = 3.14;
+        
+        double radius = perimeter / (2 * pi);
+
+        System.out.println("Radius of the circular fence: " + radius);
+    }
+}
